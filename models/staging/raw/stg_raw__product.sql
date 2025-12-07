2e80757ff8f9b31d1b63774669d559b02f58a8d4
Muge Staccioli <mstaccioli@hotmail.com>
2025-12-07 14:41:58 +0000

    latest changes

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -1,13 +1,14 @@
-with product_source as (
+with source as (
     select * from {{ source('raw', 'product') }}
 ),
 
 renamed as (
     select
         products_id,
-        CAST(purchSE_PRICE AS FLOAT64) as purchase_price
-    from product_source
+        SAFE_CAST(purchSE_PRICE AS FLOAT64) as purchase_price
+    from source
 )
 
 select * from renamed
+
 
